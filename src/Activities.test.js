a7dbb5c4114bf776ee6fe258c935e495429b30c7
Anthony Fryer <anthony.fryer@twentysixdigital.com>
2018-02-26 09:41:04 +0000

    activities dztas and tests

--- src/Activities.test.js
+++ src/Activities.test.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import Activity from './Activity';
 
 // Need to manage ajax request
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<Activities />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it('renders without crashing', () => {
+  const tableBody = document.createElement('tbody');
+  ReactDOM.render(<Activity />, tableBody);
+  ReactDOM.unmountComponentAtNode(tableBody);
+});
 
