4c37b9d6192e4208a2e19eaf6af0047e9d4bcbee
Anthony <anthonyjfryer@gmail.com>
2018-03-12 23:25:29 +0000

    added react router and movd activities

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from './components/global/Header';
-import Activities from './components/activities';
+import Activities from './components/activities/activities';
 import Athlete from './components/profile/Athlete';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -10,7 +10,6 @@ import registerServiceWorker from './registerServiceWorker';
 ReactDOM.render(<Header />, document.getElementById('header'));
 ReactDOM.render(<Activities />, document.getElementById("strava"));
 ReactDOM.render(<Athlete />, document.getElementById("athlete"));
-
 
 
 registerServiceWorker();
